93b115137b74e4e506e3990ea950b77500d163d7
KimWoojong1995 <woojong1995@gmail.com>
2021-01-05 22:48:05 +0900

    Big O Quadratic,Cubic

--- Big-O/O(n).js
+++ Big-O/O(n).js
@@ -1,5 +1,30 @@
+// 일반적인 빅오 알고리즘
+
 function exampleLinear(n) {
     for (let i = 0; i < n; i++) {
         console.log(i);
+    };
+};
+
+// O(n2)는 2차 시간이고 O(n3)은 3차시간이다
+
+function exampleQuadratic(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+        for (let j = i; j < n; j++) {
+            console.log(j);
+        };
+    };  
+};
+
+function exampleCubic(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+        for (let j = i; j < n; j++) {
+            console.log(j);
+            for (let k = j; k < n; k++) {
+                console.log(k);
+            };
+        };
     };
 };
