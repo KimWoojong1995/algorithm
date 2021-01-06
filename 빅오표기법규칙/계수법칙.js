9daf2496a9c76a53413ca5ecc6d3e8b304290ec5
KimWoojong1995 <woojong1995@gmail.com>
2021-01-06 22:33:39 +0900

    계수법칙

---
+++ 빅오표기법규칙/계수법칙.js
@@ -0,0 +1,24 @@
+function a(n) {
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        count += 1;
+    }
+    return count;
+};
+
+function a(n) {
+    let count = 0;
+    for (let i = 0; i < 5 * n; i++) {
+        count += 1;
+    }
+    return count;
+};
+
+function a(n) {
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        count += 1;
+    }
+    count += 3;
+    return count;
+};
